e5c7f59fceb6d69a90b01066d73a121dfd3e1bc5
Fabien <fabien.turgut@etu.umontpellier.fr>
2019-11-17 21:07:43 +0100

    fix front routes

--- server/routes/front.routes.js
+++ server/routes/front.routes.js
@@ -1,5 +1,5 @@
 async function frontRoutes (fastify) {
-    ["/", '/login', '/register', '/landing', '/profile/*', "/reset-password/*", "/email-vaildation/*"]
+    ["/", '/login', '/register', '/landing', '/profile/*', "/reset-password/*", "/email-validation/*"]
     .forEach(route => {
         fastify.get(route, async (req, reply) => {
             reply.code(200).sendFile("index.html");
