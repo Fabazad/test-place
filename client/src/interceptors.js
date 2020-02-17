de414615c0d8a49d438b9c2551767a6400a0b725
Unknown <fabien.turgut@etu.umontpellier.fr>
2020-02-17 23:38:04 +0100

    fix bad token redirection

--- client/src/interceptors.js
+++ client/src/interceptors.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import {toast} from 'react-toastify';
 import {getCookie} from "helpers/cookies"
+import userServices from "services/user.services";
 
 axios.interceptors.request.use(function (request) {
     request.headers['x-access-token'] = getCookie("token");
@@ -12,13 +13,21 @@ axios.interceptors.request.use(function (request) {
 axios.interceptors.response.use(function (response) {
     return response;
 }, function (error) {
+    console.log(error.response.status);
+    let message;
     if (error.response) {
-        const message = error.response.data ?
+        message = error.response.data ?
             (error.response.data.message ? error.response.data.message : error.response.data) :
             error.response.statusText;
-        toast.error(message);
     } else {
-        toast.error(error);
+        message = error;
     }
+
+    if (error.response && error.response.status && error.response.status === 401) {
+        userServices.logout();
+        window.location.replace("/");
+    }
+    toast.error(message);
+
     return Promise.reject(error);
 });
