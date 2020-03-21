d5138576d428b4044e8d6def04c21097d9c4d2ab
Fabien <fabien.turgut@gmil.com>
2020-03-21 19:16:15 +0100

    use promise for db connection

--- server/db-connection.js
+++ server/db-connection.js
@@ -3,12 +3,9 @@ require('dotenv').config();
 
 mongoose.set('useFindAndModify', false);
 async function dbConnection () {
-    mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_LOCAL_URL, {useNewUrlParser: true});
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function() {
-        console.log("DB connected");
-    });
+    mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_LOCAL_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log("DB connected"))
+        .catch(err => console.log(err));
 }
 
 module.exports = dbConnection;
