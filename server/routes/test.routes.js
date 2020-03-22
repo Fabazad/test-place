babee58c57ac9eb0a50a24a65e963a26df8c8af1
Fabien <fabien.turgut@gmil.com>
2020-03-22 03:29:16 +0100

    fix test creation

--- server/routes/test.routes.js
+++ server/routes/test.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/create', withAuth, async (request, reply) => {
     const {item} = request.body;
-    TestController.create(item, request.userId)
+    TestController.create(item, request.decoded.userId)
         .then((res) => reply.status(200).send(res))
         .catch(err => reply.status(err.status).send(err.message));
 });
