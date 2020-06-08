8eefe7afd2a40d66635fd2df981efd3e1b13c048
Fabien <fabien.turgut@gmil.com>
2020-06-08 17:01:25 +0200

    update stage url

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,8 +1,8 @@
 export default {
     SERVER_DEV_URL: 'http://127.0.0.1:5000',
-    SERVER_PROD_URL: 'https://test-place.herokuapp.com',
+    SERVER_PROD_URL: 'https://stage-test-place.herokuapp.com',
     CLIENT_DEV_URL: 'http://127.0.0.1:3000',
-    CLIENT_PROD_URL: 'https://test-place.herokuapp.com',
+    CLIENT_PROD_URL: 'https://stage-test-place.herokuapp.com',
     BASE_PRODUCT_PICTURE_URL: require("assets/img/theme/shopping-bag.png"),
     AMAZON_APP_ID: 'amzn1.application-oa2-client.1dc653b5a0d74449b587f561ea23589a',
     ITEMS_PER_PAGE: 5,
