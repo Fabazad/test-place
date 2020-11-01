358feb32bf918cfb7bd4edc350600f074c6a7afa
Fabien <fabien.turgut@gmil.com>
2020-11-01 23:32:19 +0100

    turn Headers into functional components

--- client/src/components/Headers/Header.jsx
+++ client/src/components/Headers/Header.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 
 // reactstrap components
-import { Container } from "reactstrap";
+import {Container} from "reactstrap";
 
-class Header extends React.Component {
-    render() {
-        return (
-            <>
-                <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
-                    <Container fluid>
-                        <div className="header-body">
-                          { this.props.children }
-                        </div>
-                    </Container>
+const Header = props => {
+    return (
+        <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+            <Container fluid>
+                <div className="header-body">
+                    {props.children}
                 </div>
-            </>
-        );
-    }
-}
+            </Container>
+        </div>
+    );
+};
 
 export default Header;
--- client/src/components/Headers/UserHeader.jsx
+++ client/src/components/Headers/UserHeader.jsx
@@ -2,26 +2,22 @@ import React from "react";
 
 // reactstrap components
 
-class UserHeader extends React.Component {
-  render() {
+const UserHeader = () => {
     return (
-      <>
         <div
-          className="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
-          style={{
-            minHeight: "300px",
-            backgroundImage:
-              "url(" + require("assets/img/theme/profile-cover.jpg") + ")",
-            backgroundSize: "cover",
-            backgroundPosition: "center top"
-          }}
+            className="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
+            style={{
+                minHeight: "300px",
+                backgroundImage:
+                    "url(" + require("assets/img/theme/profile-cover.jpg") + ")",
+                backgroundSize: "cover",
+                backgroundPosition: "center top"
+            }}
         >
-          {/* Mask */}
-          <span className="mask bg-gradient-default opacity-8" />
+            {/* Mask */}
+            <span className="mask bg-gradient-default opacity-8"/>
         </div>
-      </>
     );
-  }
-}
+};
 
 export default UserHeader;
